6b59ca286e8f5ca00de5bc5f55b5ca7cb5dd7f74
tonydavis629 <acdavis629@gmail.com>
2022-09-02 19:28:55 -0400

    youtube downloader

--- videorhyme.py
+++ videorhyme.py
@@ -1,7 +1,8 @@
-from videorhyme import VideoRhyme
+from videorhyme.yt_dl import VideoRhyme
 
 vr = VideoRhyme('presidential speech', num_videos=10)
 rm = vr.rhymematrix() # [n x n] triangle binary matrix of rhyming words in form (video,word_n,word)
 rhymes = vr.rhymes() # [{'id':str, 'video1':str, 'time1':float, 'word1':str, 'video2;str, 'time2':float, 'word2':str}, ...]
 videos = vr.videos(ids=rhymes[0:3].ids, window=5) # [{'video1':str, 'time1_i':float, time1f':float, 'video2':str, 'time2_i':float, 'time2_f':float}, ...]
 audio = vr.audio(ids=rhymes[0:3].ids, window=5) # [{'video1':str, 'time1_i':float, time1f':float, 'video2':str, 'time2_i':float, 'time2_f':float}, ...]
+
