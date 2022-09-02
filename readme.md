eb4644076fa00686f178442f6d517a9764609f5d
tonydavis629 <acdavis629@gmail.com>
2022-09-02 18:45:10 -0400

    init pseudocode

---
+++ readme.md
@@ -0,0 +1,4 @@
+## VideoRhyme
+
+This library takes in a search term for youtube videos and outputs slices of speech that rhyme with each other.
+
---
+++ videorhyme.py
@@ -0,0 +1,7 @@
+from videorhyme import VideoRhyme
+
+vr = VideoRhyme('presidential speech', num_videos=10)
+rm = vr.rhymematrix() # [n x n] triangle binary matrix of rhyming words in form (video,word_n,word)
+rhymes = vr.rhymes() # [{'id':str, 'video1':str, 'time1':float, 'word1':str, 'video2;str, 'time2':float, 'word2':str}, ...]
+videos = vr.videos(ids=rhymes[0:3].ids, window=5) # [{'video1':str, 'time1_i':float, time1f':float, 'video2':str, 'time2_i':float, 'time2_f':float}, ...]
+audio = vr.audio(ids=rhymes[0:3].ids, window=5) # [{'video1':str, 'time1_i':float, time1f':float, 'video2':str, 'time2_i':float, 'time2_f':float}, ...]
