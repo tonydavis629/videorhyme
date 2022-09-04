7f4cb9fa8bbb54586fbfb63f3593e956d3d303c9
tonydavis629 <acdavis629@gmail.com>
2022-09-04 18:10:19 -0400

    download captions, init sp2txt

--- videorhyme/sp2txt/speech2txt.py
+++ videorhyme/sp2txt/speech2txt.py
@@ -1,4 +1,45 @@
-from speechbrain.pretrained import EncoderDecoderASR
+import torch
+import torchaudio
 
-asr_model = EncoderDecoderASR.from_hparams(source="speechbrain/asr-wav2vec2-commonvoice-en", savedir="pretrained_models/asr-wav2vec2-commonvoice-en")
-asr_model.transcribe_file("/home/tony/github/videorhyme/videos/Biden blasts 'MAGA Republicans' as a threat to democracy/Biden blasts MAGA Republicans as a threat to democracy.webm")
+torch.random.manual_seed(0)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+SPEECH_FILE = ""
+
+bundle = torchaudio.pipelines.WAV2VEC2_ASR_BASE_960H
+
+model = bundle.get_model().to(device)
+
+class GreedyCTCDecoder(torch.nn.Module): 
+    def __init__(self, labels, blank=0):
+        super().__init__()
+        self.labels = labels
+        self.blank = blank
+
+    def forward(self, emission: torch.Tensor) -> str:
+        """Given a sequence emission over labels, get the best path string
+        Args:
+          emission (Tensor): Logit tensors. Shape `[num_seq, num_label]`.
+
+        Returns:
+          str: The resulting transcript
+        """
+        indices = torch.argmax(emission, dim=-1)  # [num_seq,]
+        indices = torch.unique_consecutive(indices, dim=-1)
+        indices = [i for i in indices if i != self.blank]
+        return "".join([self.labels[i] for i in indices])
+    
+waveform, sample_rate = torchaudio.load(SPEECH_FILE)
+waveform = waveform.to(device)
+
+if sample_rate != bundle.sample_rate:
+    waveform = torchaudio.functional.resample(waveform, sample_rate, bundle.sample_rate)
+    
+with torch.inference_mode():
+    emission, _ = model(waveform)
+    
+decoder = GreedyCTCDecoder(labels=bundle.get_labels())
+transcript = decoder(emission[0])
+
+
+print(transcript)
--- videorhyme/youtube_dl/downloader.py
+++ videorhyme/youtube_dl/downloader.py
@@ -1,25 +1,39 @@
 import os
 from pytube import YouTube
 import subprocess
+from xml.etree import ElementTree
+from html import unescape
 
 class Downloader:
     def __init__(self, link:str, title:str, save_dir:str = ''):
         self.link = link
-        self.title = title.replace(' ', '_').replace('\"','').replace('\'','')
+        self.title = title.replace(' ', '_').replace('\"','').replace('\'','').replace('|','')
         self.save_dir = save_dir
 
     def download(self):
         yt = YouTube(self.link)
         yv = yt.streams.filter(only_video=True, adaptive=True, file_extension='mp4').order_by('resolution').desc().first() #video stream with highest resolution
-        ya = yt.streams.filter(only_audio=True, adaptive=True).order_by('abr').desc().first() #audio stream with highest resolutionself.titleself.title
+        ya = yt.streams.filter(only_audio=True).order_by('abr').desc().first() #audio stream with highest resolutionself.titleself.title
+        yc = yt.captions['en'] #english captions
 
-        path = os.path.join(self.save_dir, self.title).replace(' ', '_').replace('\"','\'').replace('\'','')
+        path = os.path.join(self.save_dir, self.title).replace(' ', '_').replace('\"','\'').replace('\'','').replace('|','')
+        
+        #download video
         yv.download(output_path = path, filename = self.title + '.mp4')
+        
+        #download audio, convert to wav
         ya.download(output_path = path, filename = self.title + '.webm')
-          
         audio_path = os.path.join(path, self.title)
-        command = f"ffmpeg -i {audio_path}.webm -ab 160k -ac 2 -ar 44100 -vn {audio_path}.wav"
+        webm2wav(audio_path)
         
-        subprocess.call(command, shell=True)
+        #download captions as srt 
+        yc.download(output_path = path, title = self.title + '.srt', srt=True)
+
         
-        os.remove(audio_path + '.webm')
+def webm2wav(audio_path):
+    command = f"ffmpeg -i {audio_path}.webm -ab 160k -ac 2 -ar 44100 -vn {audio_path}.wav"
+    
+    subprocess.call(command, shell=True)
+    
+    os.remove(audio_path + '.webm')
+    
