e801b89ba6754a71a1154dff9b943e476528771d
Tony Davis <acdavis629@gmail.com>
2022-09-17 18:55:13 -0400

    WIP rhyme matrix

--- videorhyme.py
+++ videorhyme.py
@@ -1,7 +1,7 @@
 # from videorhyme.yt_dl import VideoRhyme
 from videorhyme.youtube_dl.yt_dl import yt_dl
 
-downloader = yt_dl(query = 'protest', num_videos = 3, save_dir='videos')
+downloader = yt_dl(query = 'protest', num_videos = 5, save_dir='videos')
 downloader.download()
 
 # vr = VideoRhyme('presidential speech', num_videos=10)
--- videorhyme/rhymer/rhymer.py
+++ videorhyme/rhymer/rhymer.py
@@ -1,11 +1,13 @@
 import numpy as np
 import glob
 import os
+import nltk
 
 class rhymer():
     def __init__(self, srts_dir:str):
         self.videos, self.scripts = self.load_srts(srts_dir)
-        self.words = self.get_words() # get all words to rhyme with
+        self.words, self.lookup = self.get_words() # get all words to rhyme with
+        self.rhyme_matrix = self.rhyme_matrix()
     def load_srts(self, srts_dir:str):
         #find the paths of the srt files
         # srt_paths = glob.glob(srts_dir + '/*/*.srt')
@@ -35,14 +37,45 @@ class rhymer():
                 scripts.append(script)
         return videos, scripts
     def get_words(self):
-        """Take the scripts, pull out the last word of each phrase, and return a list of all words"""
+        """Take the scripts, pull out the relevant words from each script, and return a list of all words in one object"""
         words = []
-        for script in self.scripts:
+        lookup = []
+        for i,script in enumerate(self.scripts):
+            video = self.videos[i]
             for cap_set in script:
+                start = cap_set[0]
+                stop = cap_set[1]
                 text = cap_set[2]
-                words.append(text.split(' ')[-1])
-        return words
+                phrase = text.split(' ')
+                for word in phrase:
+                    lookup.extend((video,start,stop))
+                words.extend(phrase)
+        return words, lookup
+    
+    def rhyme(self, inp, level):
+        entries = nltk.corpus.cmudict.entries()
+        syllables = [(word, syl) for word, syl in entries if word == inp]
+        rhymes = []
+        for (word, syllable) in syllables:
+                rhymes += [word for word, pron in entries if pron[-level:] == syllable[-level:]]
+        return set(rhymes)
+    
+    def rhymescore(self,word1:str,word2:str):
+        """Takes in two words and rates how well they rhyme"""
+        if word1.find(word2) == len(word1) - len(word2):
+            return False
+        if word2.find(word1) == len(word2) - len(word1): 
+            return False
+        return word1 in self.rhyme(word2, 1)
+        
+    def rhyme_matrix(self):
+        num_words = sum([len(i) for i in self.words])
+        rhy_mat = np.zeros((num_words,num_words))
+        for x,row in enumerate(rhy_mat):
+            for y,val in enumerate(row):
+                rhy_mat[x,y] = self.rhymescore(self.words(x),self.words(y))
+                
+        return rhy_mat
         
 rh = rhymer('/home/tony/github/videorhyme/videos')
-rh.videos
-rh.scripts
+print(rh.rhyme_matrix())
