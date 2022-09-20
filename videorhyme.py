08d4a94a89c5456ecea6a783016dffe13be1d373
Tony Davis <acdavis629@gmail.com>
2022-09-19 22:19:50 -0400

    videorhyme class init

--- videorhyme.py
+++ videorhyme.py
@@ -5,7 +5,3 @@ downloader = yt_dl(query = 'protest', num_videos = 5, save_dir='videos')
 downloader.download()
 
 # vr = VideoRhyme('presidential speech', num_videos=10)
-# rm = vr.rhymematrix() # [n x n] triangle binary matrix of rhyming words in form (video,word_n,word)
-# rhymes = vr.rhymes() # [{'id':str, 'video1':str, 'time1':float, 'word1':str, 'video2;str, 'time2':float, 'word2':str}, ...]
-# videos = vr.videos(ids=rhymes[0:3].ids, window=5) # [{'video1':str, 'time1_i':float, time1f':float, 'video2':str, 'time2_i':float, 'time2_f':float}, ...]
-# audio = vr.audio(ids=rhymes[0:3].ids, window=5) # [{'video1':str, 'time1_i':float, time1f':float, 'video2':str, 'time2_i':float, 'time2_f':float}, ...]
---
+++ videorhyme/videorhymer.py
@@ -0,0 +1,8 @@
+from youtube_dl.yt_dl import yt_dl
+
+class videorhymer():
+    def __init__():
+        pass
+    def download(self):
+        pass
+    
